e87afe0ef38f15907d29f1b2dd075fa23fc7d639
oktntko <oktntko@gmail.com>
2022-06-05 18:43:44 +0900

    [react] Update your configuration file to eliminate this warning.

--- todo-react/tailwind.config.js
+++ todo-react/tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
