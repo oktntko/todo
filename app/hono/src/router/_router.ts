2bf62346ab702b29e0f10e61970f4ac17dd22998
oktntko <oktntko@gmail.com>
2025-10-08 22:22:50 +0900

    refactor: remove hono/react app

--- app/hono/src/router/_router.ts
+++
@@ -1,6 +0,0 @@
-import { Hono } from "hono";
-import { UserRouter } from "./UserRouter.js";
-
-export const HonoRouter = new Hono().route("/", UserRouter);
-
-export type TypeHonoRouter = typeof HonoRouter;
